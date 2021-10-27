b84bd561aae3520af6bf1f5d9700950eae7d916e
Bogdan Radulescu <Bogdan.Radulescu@softwire.com>
2021-10-27 11:52:22 +0300

    exercise-1: added functionality to add/remove items to todo

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
-
+import flask
 from todo_app.flask_config import Config
+import todo_app.data.session_items as si
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config.from_object(Config())
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo_list = si.get_items()
+    return flask.templating.render_template("index.html", todo_list=todo_list)
+
+@app.route('/add-todo', methods=["POST"])
+def add_todo():
+    todo_name = flask.request.form.get('todo_name')
+    si.add_item(todo_name)
+
+    response = flask.redirect('/')
+    response.autocorrect_location_header = False
+
+    return response
+
+@app.route('/remove-todo', methods=["POST"])
+def remove_todo():
+    id = flask.request.form.get('id')
+    si.remove_item(id)
+
+    response = flask.redirect('/')
+    response.autocorrect_location_header = False
+    
+    return response
